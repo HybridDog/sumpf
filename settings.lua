083ca552a25ec9bbe84fbfcdb491a0b512ee4312
HybridDog <adresse_test@t-online.de>
2013-03-30 16:48:36 +0100

    disable mapgen as default

--- settings.lua
+++ settings.lua
@@ -1,7 +1,7 @@
 --This file contains configuration options for swamp mod.
 
 --Enables mapgen.
-sumpf.enable_mapgen = true
+sumpf.enable_mapgen = false
 
 --Enables smooth transition of biomes.
 sumpf.smooth = true
